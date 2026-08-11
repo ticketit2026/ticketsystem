36f7bb3f775256b632bee7c72a82bdd0b60282ea
ticketit2026 <ticketit2026@gmail.com>
2026-08-11 09:48:22 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,3 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python create_admin.py
-if [ "$CREATE_SUPERUSER" = "True" ]; then
-  python manage.py createsuperuser --noinput
-fi
